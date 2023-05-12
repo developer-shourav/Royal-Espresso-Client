e120524adadd000516bb6c42cd75d75a4be83756
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-12 07:05:51 +0600

    primary routing added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App> </App>
+  },
+  {
+    path: '/about',
+    element: <h2>Here is about me</h2>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}> 
+
+   </RouterProvider>
   </React.StrictMode>,
 )
