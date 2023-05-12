99840dd47c610e9ff5f41a0af4645a0f58c14adb
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-12 15:59:01 +0600

    dynamic coffee added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import CoffeeCard from "./components/CoffeeCard/CoffeeCard";
 
 function App() {
   const allCoffees = useLoaderData();
   console.log(allCoffees);
   return (
-    <>
-       <h2 className="text-3xl text-center">Total Coffees: {allCoffees?.length}</h2>
-    </>
+    <div className="my-10 container mx-auto">
+       <h2 className="text-3xl text-center mb-5">Total Coffees: {allCoffees?.length}</h2>
+
+       {/* ----------Coffee Card--------- */}
+       <div className="grid md:grid-cols-2 gap-8 ">
+         {
+          allCoffees.map( coffee => <CoffeeCard
+          key={coffee?._id}
+          coffee={coffee}
+          > 
+
+          </CoffeeCard>)
+         }
+       </div>
+    </div>
   );
 }
 
--- src/components/CoffeeCard/CoffeeCard.jsx
+++ src/components/CoffeeCard/CoffeeCard.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 
-const CoffeeCard = () => {
+const CoffeeCard = ({coffee}) => {
+  const {name, quantity, supplier, taste, category, details, photo} = coffee;
   return (
     <div>
       <div className="card card-side bg-base-100 shadow-xl">
         <figure>
           <img
-            src="/images/stock/photo-1635805737707-575885ab0820.jpg"
-            alt="Movie"
+            src={photo}
+            alt="Coffee"
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">New movie is released!</h2>
-          <p>Click the button to watch on Jetflix app.</p>
+          <h2 className="card-title">Name: {name} </h2>
+          <p>{details}</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Watch</button>
           </div>
