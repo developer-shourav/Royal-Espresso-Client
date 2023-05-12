c4f50be3d976e0e55039147d000faa8e6dd1b5ab
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-12 12:18:26 +0600

    coffee adding form added

--- src/components/AddCoffee/AddCoffee.jsx
+++ src/components/AddCoffee/AddCoffee.jsx
@@ -1,14 +1,145 @@
-import React, { useEffect } from 'react';
-import useTitle from '../../assets/hooks/useTitle';
+import React, { useEffect } from "react";
+import useTitle from "../../assets/hooks/useTitle";
 
 const AddCoffee = () => {
-    // For Dynamic Title
-    useTitle('AddCoffee')
-    return (
-        <div>
-            <h1>Add Your Coffee</h1>
+  // For Dynamic Title
+  useTitle("AddCoffee");
+  return (
+    <div className="bg-[#F4F3F0] p-24">
+      <h2 className="text-3xl font-extrabold">Add A Coffee</h2>
+
+      <form>
+        {/* ----------Name and Quantity Row---------- */}
+        <div className="md:flex">
+          <div className="form-control w-1/2">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Coffee Name"
+                name="name"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+
+          <div className="form-control w-1/2 ml-4">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="quantity"
+                placeholder="Available Quantity"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+
+
         </div>
-    );
+        {/* ----------From Row---------- */}
+        <div className="md:flex">
+          <div className="form-control w-1/2">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Coffee Name"
+                name="name"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+
+          <div className="form-control w-1/2 ml-4">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="quantity"
+                placeholder="Available Quantity"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+
+
+        </div>
+        {/* ----------From Row---------- */}
+        <div className="md:flex">
+          <div className="form-control w-1/2">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Coffee Name"
+                name="name"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+
+          <div className="form-control w-1/2 ml-4">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="quantity"
+                placeholder="Available Quantity"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+
+
+        </div>
+        {/* ----------From Row---------- */}
+        <div className="md:flex">
+          <div className="form-control w-1/2">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Coffee Name"
+                name="name"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+
+          <div className="form-control w-1/2 ml-4">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="quantity"
+                placeholder="Available Quantity"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+
+
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddCoffee;
