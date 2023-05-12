59ad7958918963a1608fba602d56c56c53745417
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-12 18:43:19 +0600

    after delete remaingin added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { useLoaderData } from "react-router-dom";
 import CoffeeCard from "./components/CoffeeCard/CoffeeCard";
 
 function App() {
-  const allCoffees = useLoaderData();
-  const [coffees, setCoffees] = useState(allCoffees)
+  const loadedCoffees = useLoaderData();
+  const [coffees, setCoffees] = useState(loadedCoffees)
   return (
     <div className="my-10 container mx-auto">
-       <h2 className="text-3xl text-center font-bold mb-10">Total Coffees: {allCoffees?.length}</h2>
+       <h2 className="text-3xl text-center font-bold mb-10">Total Coffees: {loadedCoffees?.length}</h2>
 
        {/* ----------Coffee Card--------- */}
        <div className="grid md:grid-cols-2 gap-8 ">
@@ -15,6 +15,8 @@ function App() {
           coffees?.map( coffee => <CoffeeCard
           key={coffee?._id}
           coffee={coffee}
+          coffees={coffees}
+          setCoffees={setCoffees}
           > 
 
           </CoffeeCard>)
--- src/components/CoffeeCard/CoffeeCard.jsx
+++ src/components/CoffeeCard/CoffeeCard.jsx
@@ -5,7 +5,7 @@ import {HiPencil} from 'react-icons/hi';
 import Swal from 'sweetalert2'
 import { Link } from "react-router-dom";
 
-const CoffeeCard = ({ coffee }) => {
+const CoffeeCard = ({ coffee, coffees, setCoffees }) => {
   const { _id ,name, quantity, supplier, taste, category, details, photo } = coffee;
 
 
@@ -30,13 +30,15 @@ const CoffeeCard = ({ coffee }) => {
         } )
         .then( res => res.json())
         .then( data => {
-          console.log(data);
           if(data.deletedCount > 0){
             Swal.fire(
               'Item Deleted!',
               'Your Coffee Deleted Successfully.',
               'success'
             )
+
+            const remainingCoffees = coffees.filter( cof => cof._id !== id);
+            setCoffees(remainingCoffees)
           }
         })
        
